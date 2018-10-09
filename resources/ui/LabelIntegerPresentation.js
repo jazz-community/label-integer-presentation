76b34628a38e157cd76720b2d65c49a97768bd71
MartinBenninger <martin.benninger@outlook.com>
2018-10-09 17:35:24 +0200

    Add initial widget

---
+++ resources/ui/LabelIntegerPresentation.js
@@ -0,0 +1,11 @@
+dojo.require("com.ibm.team.workitem.web.ui.internal.view.editor.presentations.attribute.IntegerPresentation");
+
+define([
+    "dojo/_base/declare"
+], function (declare) {
+    return declare("com.siemens.bt.jazz.workitemeditor.presentation.labelIntegerPresentation.ui.LabelIntegerPresentation",
+        com.ibm.team.workitem.web.ui.internal.view.editor.presentations.attribute.IntegerPresentation,
+    {
+
+    });
+});

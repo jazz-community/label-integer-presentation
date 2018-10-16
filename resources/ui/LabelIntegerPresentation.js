772b8f7e21d8c25f75e6f09ebae70194121f098c
MartinBenninger <martin.benninger@outlook.com>
2018-10-16 13:00:04 +0200

    Align the value to the right

--- resources/ui/LabelIntegerPresentation.js
+++ resources/ui/LabelIntegerPresentation.js
@@ -21,6 +21,35 @@ dojo.require("com.ibm.team.workitem.web.internal.registry.PresentationRegistry")
 
         getId: function () {
             return presentationId;
+        },
+
+        // Override the create view function to customize the result
+        createView: function (context, params, domNode) {
+            console.log("createView arguments", arguments);
+            var view = this.inherited(arguments);
+
+            if (context.editorPresentation.isReadOnly()) {
+                console.log("view is readonly");
+                this._alignTextRight(view._value);
+            } else {
+                console.log("view is editable");
+                this._setInputType(view, "number");
+                this._alignTextRight(view._input);
+            }
+
+            console.log("view", view);
+            return view;
+        },
+
+        // Set the view input to the specified type
+        _setInputType: function (view, inputType) {
+            view.type = inputType;
+            view._input.type = inputType;
+        },
+
+        // Align the text of the specified element to the right
+        _alignTextRight: function (element) {
+            dojo.style(element, "textAlign", "right");
         }
     });
 
